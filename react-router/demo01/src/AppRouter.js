a75ab7cefb9981957e6d3b27f698aa45af879d54
jcs <rosejcs@163.com>
2019-10-05 23:58:31 +0800

    start react-router 1005

---
+++ react-router/demo01/src/AppRouter.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Index from './Pages/Index'
+import List from './Pages/List'
+
+
+// function Index() {
+//   return <h2>JSPANG.com</h2>
+// }
+
+// function List() {
+//   return <h2>List-page</h2>
+// }
+
+// 设置规则 --- 传递值 --- 接收值--- 显示内容
+
+
+
+function AppRouter() {
+  return (
+
+    <Router>
+      <ul>
+        <li><Link to="/">首页</Link></li>
+        <li><Link to="/list/345">列表</Link></li>
+      </ul>
+      {/* exact 精确匹配,一般首页需要 */}
+      <Route path="/" exact component={Index} />
+      <Route path="/list/:id" component={List} />
+    </Router>
+  )
+}
+
+export default AppRouter
---
+++ react-router/demo01/src/Pages/Index.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        { cid: 123, title: "技术胖的个人博客-1" },
+        { cid: 456, title: "技术胖的个人博客-2" },
+        { cid: 789, title: "技术胖的个人博客-3" }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>JSPANG.com</h2>
+        <ul>
+          {this.state.list.map((item, index) => {
+            return (
+              <li key={index + item}>
+                <Link to={'/list/'+item.cid}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Index;
