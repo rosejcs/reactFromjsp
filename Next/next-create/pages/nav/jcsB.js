9655c252e6706b5a8b849fbb35c8b2a77ffc5b4a
rosejcs <rosejcs@163.com>
2019-10-19 02:41:49 +0800

    next page component

---
+++ Next/next-create/pages/nav/jcsB.js
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+export default ()=>(
+  <>
+    <h1>B page</h1>
+    <Link href="/">go Index</Link>
+  </>
+)
---
+++ Next/next-create/pages/nextTab/nextJcs.js
@@ -0,0 +1,1 @@
+export default () => <button>NextJcs page</button>
