c4e94f2c7d1971ade857db5acea3987950f0b630
rosejcs <rosejcs@163.com>
2019-10-22 03:25:24 +0800

    reactProject 1022morning

---
+++ reactProject/blog/components/Advert.js
@@ -0,0 +1,13 @@
+import '../public/style/components/advert.css'
+
+const Advert = () => {
+  return (
+    <div className="ad-div comm-box">
+      <div><img src="http://blogimages.jspang.com/flutter_ad2.jpg" width="100%" alt="1"/></div>
+      <div><img src="http://blogimages.jspang.com/Vue_koa_ad1.jpg" width="100%" alt="2"/></div>
+      <div><img src="http://blogimages.jspang.com/WechatIMG12.jpeg" width="100%" alt="3"/></div>
+      <div><img src="https://jspang.com/images/ad_4.jpg" width="100%" alt="4"/></div>
+    </div>
+  )
+}
+export default Advert
